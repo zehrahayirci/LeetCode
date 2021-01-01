5690efaff17a4ce5279da476062a1fc6cf99a406
zehra.hayirci <zehra.hayirci@helmholtz-muenchen.de>
2021-01-01 19:15:24 +0100

    01.01.2020

---
+++ easy/1_two_sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range( len(nums) ):
+            for j in range(i+1,len(nums)):
+                print("trial", i , j)
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+S= Solution()
+print(S.twoSum([0,1,2],3))
