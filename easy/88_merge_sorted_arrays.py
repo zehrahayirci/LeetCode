8accf7b43d96289fd824192e89f4751ab430e6ef
zehra.hayirci <zehra.hayirci@helmholtz-muenchen.de>
2021-01-18 23:19:35 +0100

    18.01.2021

---
+++ easy/88_merge_sorted_arrays.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+ 
+        k = m+n-1
+        i = m-1
+        j = n-1
+        while k>-1 :
+            if i == - 1:
+                nums1[k] = nums2[j]
+                j-=1
+            elif j==-1:
+                nums1[k] = nums1[i]
+                i-=1            
+            elif nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i-=1
+            else:
+                nums1[k] = nums2[j]
+                j-=1
+            k-=1
+        
