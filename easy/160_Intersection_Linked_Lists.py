58ae05333d4d0a5ba0d8b1b90dbfaf7033c26fad
zehra.hayirci <zehrahayirci@gmail.com>
2021-05-12 16:19:33 +0200

    12.05.2021

--- easy/160_Intersection_Linked_Lists.py
+++ easy/160_Intersection_Linked_Lists.py
@@ -19,4 +19,42 @@ class Solution(object):
         return cur_A    
         #They will not hit the first round but the second one! 
         #because we skip the first unmatched parts 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        countA = 0
+        curA = headA
+        while(curA):
+            curA = curA.next
+            countA+=1
+        countB = 0
+        curB = headB
+        while(curB):
+            curB = curB.next
+            countB+=1
+        print(countA,countB)
+        fark = abs(countA-countB)
+        if countA>countB:
+            curA=headA
+            curB=headB
+        else:
+            curA=headB
+            curB=headA
+        i = 0
+        while i < fark:
+            curA = curA.next
+            i+=1
+        print(i)
+        while(curA != curB):
+            curA=curA.next
+            curB=curB.next
+        
+        return curB
+            
    
